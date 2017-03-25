3c1f6d3b43eb9a569bed8d4520e2a038fba87bec
Mark <jungtaehoon@icloud.com>
2017-03-25 02:56:18 -0700

    complete unSortedArrayMaker function

--- sort.js
+++ sort.js
@@ -1,3 +1,16 @@
-var unSortedArrayMaker = function(length) {
-  
+const unSortedArrayMaker = function(length) {
+  let arr = [];
+  let m = 0;
+  for (let i = 0; i < length; i++) {
+    arr.push(i);
+  }
+  while(m < length) {
+    let randomIndex = Math.floor(Math.random() * (length - m) + m);
+    let temp = arr[randomIndex];
+    arr[randomIndex] = arr[m];
+    arr[m] = temp;
+    m++;
+  }
+  return arr;
 }
+//console.log(unSortedArrayMaker(1000));
