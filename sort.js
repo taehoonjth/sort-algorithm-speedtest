5da26f2454c59cf85cd12ef7fbc7a67fa5f53c1f
Mark <jungtaehoon@icloud.com>
2017-03-27 01:52:34 -0700

    implement bubbleSort

--- sort.js
+++ sort.js
@@ -13,4 +13,35 @@ const unSortedArrayMaker = function(length) {
   }
   return arr;
 }
+
+const bubbleSort = function(array) {
+  if (array.length < 2) {
+    return array.slice();
+  }
+  const start = new Date();
+  let result = array.slice();
+  let end = result.length;
+  while (end !== 1) {
+    for (var i = 0; i < end - 1; i++) {
+      let a = result[i];
+      let b = result[i + 1];
+      if (a > b) {
+        result[i] = b;
+        result[i + 1] = a;
+      }
+    }
+    end--;
+  }
+  const done = new Date();
+  console.log(`time ${done - start}`);
+  return result;
+}
+
+var arr = unSortedArrayMaker(10000);
+
+console.log(bubbleSort(arr));
+
+
+
+
 //console.log(unSortedArrayMaker(1000));
